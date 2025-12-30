8509dedddd20219c3779614c9596016687e27bca
AlbaYS11 <adrianalbamiranda@gmail.com>
2025-12-29 18:26:09 -0600

    Mi primer commit

---
+++ clase0.cpp
@@ -0,0 +1,15 @@
+#include <iostream> // Library for Input/Output
+
+using namespace std;
+
+// Main function: The entry point of any C++ program
+int main() {
+    // Variable declaration (Declaramos una variable entera)
+    int semestre = 4;
+
+    // Output to console (Imprimimos en pantalla)
+    cout << "Hello World!" << endl;
+    cout << "I am ready for semester " << semestre << endl;
+
+    return 0; // Program finished successfully
+}
