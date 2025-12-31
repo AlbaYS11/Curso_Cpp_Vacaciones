deb37d6247c2182d8ae77d933513218e5651c1eb
AlbaYS11 <adrianalbamiranda@gmail.com>
2025-12-30 22:57:31 -0600

    Clase 1 Ifs

--- clase0.cpp
+++ clase0.cpp
@@ -1,33 +1,48 @@
 #include <iostream> //Para poder usar System input y output osea cin y cout
-#include <string> //Para poder usar strings comodamente
-//Import
+#include <string>   //Para poder usar strings comodamente
+// Import
 
-using namespace std; //Para poder usar cin, cout y string sin alias, es la caja de herramientas
+using namespace std; // Para poder usar cin, cout y string sin alias, es la caja de herramientas
 
-int main(){
+int main()
+{
 
-int edad;
-string nombre;    
-double precio;
+    int edad;
+    string nombre;
+    double precio;
 
-cout << "¡Bienvenido! \nPara entrar haz el registro." << endl;
-cout << "Ingresa tu nombre:" << endl;
-cin >> nombre; //Corta espacios
+    cout << "¡Bienvenido! \nPara entrar haz el registro." << endl;
+    cout << "Ingresa tu nombre:" << endl;
+    cin >> nombre; // Corta espacios
 
-cout << "Ingresa tu edad:" << endl;
-cin >> edad;
+    cout << "Ingresa tu edad:" << endl;
+    cin >> edad;
 
-cout << "Ingresa el precio de tu entrada:" << endl;
-cin >> precio;
+    if (edad >= 18)
+    {
 
-cout << "\nResumen del ticket:\nNombre: " << nombre << "\nEdad: " << edad << 
-"\nA pagar: "<< precio << endl;
+        cout << "Ingresa el precio de tu entrada:" << endl;
+        cin >> precio;
 
-return 0;
+        if (precio < 150)
+        {
 
+            cout << "Te falta varo, consigue más." << endl;
+        }
+        else
+        {
+
+            cout << "\nResumen del ticket:\nNombre: " << nombre << "\nEdad: " << edad << "\nA pagar: " << precio << endl;
+        }
+    }
+    else
+    {
+        cout << "¡Largo de aqui, niño!" << endl;
+    }
+
+    return 0;
+}
 // ¿Por qué pasa? cin es flojo. Lee caracteres hasta que encuentra un espacio en blanco (espacio, tabulador o enter)
 // y ahí se detiene, pensando que ya acabó el dato.
-//Solución futura: Usaremos getline(cin, variable) 
-//más adelante para leer frases enteras. Por ahora, cin es suficiente.
-
-}
+// Solución futura: Usaremos getline(cin, variable)
+// más adelante para leer frases enteras. Por ahora, cin es suficiente.
