11db564758dc1f0cee1110b5e14f6cf9353ec9eb
AlbaYS11 <adrianalbamiranda@gmail.com>
2025-12-30 22:30:40 -0600

    Clase 1 Input y output basico

--- clase0.cpp
+++ clase0.cpp
@@ -1,17 +1,33 @@
-#include <iostream> // Library for Input/Output
+#include <iostream> //Para poder usar System input y output osea cin y cout
+#include <string> //Para poder usar strings comodamente
+//Import
 
-using namespace std;
+using namespace std; //Para poder usar cin, cout y string sin alias, es la caja de herramientas
 
-// Main function: The entry point of any C++ program
-int main() {
-    // Variable declaration (Declaramos una variable entera)
-    int semestre = 4;
+int main(){
 
-    // Output to console (Imprimimos en pantalla)
-    cout << "Hello World!" << endl;
-    cout << "I am ready for semester " << semestre << endl;
+int edad;
+string nombre;    
+double precio;
 
-    cout << "Jala Porfa" << endl;
+cout << "¡Bienvenido! \nPara entrar haz el registro." << endl;
+cout << "Ingresa tu nombre:" << endl;
+cin >> nombre; //Corta espacios
 
-    return 0; // Program finished successfully
+cout << "Ingresa tu edad:" << endl;
+cin >> edad;
+
+cout << "Ingresa el precio de tu entrada:" << endl;
+cin >> precio;
+
+cout << "\nResumen del ticket:\nNombre: " << nombre << "\nEdad: " << edad << 
+"\nA pagar: "<< precio << endl;
+
+return 0;
+
+// ¿Por qué pasa? cin es flojo. Lee caracteres hasta que encuentra un espacio en blanco (espacio, tabulador o enter)
+// y ahí se detiene, pensando que ya acabó el dato.
+//Solución futura: Usaremos getline(cin, variable) 
+//más adelante para leer frases enteras. Por ahora, cin es suficiente.
+
 }
